5c1ade82e38403aa47062b65cfedd8e94f21eabe
Ben Wray <c3021537@newcastle.ac.uk>
2025-03-06 19:13:58 +0000

    added mySQL database

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,4 @@
-﻿from flask import Flask
-
-app = Flask(__name__)
+﻿from config import app
 
 
 @app.route('/')
---
+++ flaskr/config.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost:3306/database'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
