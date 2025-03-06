6fb4642e9e63a257b457c429ea4db7c693b0d776
Ben Wray <c3021537@newcastle.ac.uk>
2025-03-06 19:20:42 +0000

    added mySQL database to docker-compose.yaml

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,11 +1,10 @@
 ﻿from flask import Flask
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost:3306/database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:password@localhost:3306/mysql-db'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
