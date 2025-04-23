7e7da54f3167784b6fbe450116e317f3508bf8fd
somerandomguy101 <c3029594@newcastle.ac.uk>
2025-04-23 12:05:09 +0100

    created a method in mail.py which sends an email generated from arguments which can be imported into other files in the project

--- flaskr/mail.py
+++ flaskr/mail.py
@@ -11,7 +11,28 @@ load_dotenv()
 # Mailjet SMTP credentials
 smtp_username = os.getenv('SMTP_USERNAME')
 smtp_password = os.getenv('SMTP_SECRET')
+from_email = 'IIIinsight@outlook.com'
 
+def send_email(reciever_email, subject, body):
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = reciever_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        # Connect to Mailjet SMTP server
+        server = smtplib.SMTP('in-v3.mailjet.com', 587)
+        server.starttls()
+        server.login(smtp_username, smtp_password)
+        server.sendmail(from_email, reciever_email, msg.as_string())
+        server.quit()
+        print('✅ Email sent successfully!')
+    except Exception as e:
+        print(f'❌ Failed to send email: {e}')
+
+
+"""
 # Email content
 from_email = 'IIIinsight@outlook.com'   # must match your verified sender
 to_email = 'alexsutcliffe44@gmail.com'
@@ -35,3 +56,4 @@ try:
     print('✅ Email sent successfully!')
 except Exception as e:
     print(f'❌ Failed to send email: {e}')
+"""
