c0a94985d8a5af2f7d41e217c62dff152a1c13ca
somerandomguy101 <c3029594@newcastle.ac.uk>
2025-04-23 12:06:50 +0100

    deleted initial email test script from mail.py

--- flaskr/mail.py
+++ flaskr/mail.py
@@ -31,29 +31,3 @@ def send_email(reciever_email, subject, body):
     except Exception as e:
         print(f'❌ Failed to send email: {e}')
 
-
-"""
-# Email content
-from_email = 'IIIinsight@outlook.com'   # must match your verified sender
-to_email = 'alexsutcliffe44@gmail.com'
-subject = 'Hello from Mailjet + Python!'
-body = 'This is a test email sent through Mailjet using Python.'
-
-# Build the email
-msg = MIMEMultipart()
-msg['From'] = from_email
-msg['To'] = to_email
-msg['Subject'] = subject
-msg.attach(MIMEText(body, 'plain'))
-
-try:
-    # Connect to Mailjet SMTP server
-    server = smtplib.SMTP('in-v3.mailjet.com', 587)
-    server.starttls()
-    server.login(smtp_username, smtp_password)
-    server.sendmail(from_email, to_email, msg.as_string())
-    server.quit()
-    print('✅ Email sent successfully!')
-except Exception as e:
-    print(f'❌ Failed to send email: {e}')
-"""
