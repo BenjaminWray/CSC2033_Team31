6229c3ac4fe5b4e5362e1ed4786a4e53565ee0da
shan <c3029785@newcastle.ac.uk>
2025-05-06 20:04:24 +0100

    Refector signup: optional location dropdown

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone_number = StringField('Phone Number')
-    location = StringField('Location (e.g., Newcastle, UK)')
+    location = SelectField('Location (optional)', choices=[
+        ('', 'Select your location'),
+        ('newcastle', 'Newcastle, UK'),
+        ('london', 'London, UK'),
+        ('manchester', 'Manchester, UK'),
+        ('edinburgh', 'Edinburgh, UK'),
+    ], validators=[])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
